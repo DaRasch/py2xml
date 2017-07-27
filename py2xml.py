060397c89f83cd3677e679a4d6a30ea05b7d1538
Philipp Rasch <ph.r@hotmail.de>
2017-07-27 16:07:28 +0200

    Fix and extend example

--- py2xml.py
+++ py2xml.py
@@ -8,19 +8,41 @@ The only exported objects are the `Document` and `ContextStack` classes.
 Example
 =======
 
-class Page(Document):
+from py2xml import Document, ContextStack
+
+def dynamic_subtree(id, text):
+    # Add the content of SubPage to the current context
+    class SubPage(Document, blacklist=['text', 'id', 'str']):
+        # The blacklist protects names of arguments and external functions
+        with div(id=str(id), **{'class': 'someclass'}) as root:
+            p(text)
+    return SubPage.root
+
+def split_context(id, text):
+    # Create a seperate document for SubPage
+    class SubPage(Document, whitelist=['div', 'p'], ctx=ContextStack()):
+        # The whitelist limits the available names
+        with div(id=str(id), **{'class': 'someclass'}) as root:
+            p(text)
+    return SubPage.root
+
+class Page(Document, blacklist=dir()):
+    # blacklist=dir() is handy do be able to acces all defined names
     # Simple html document
     with html() as root:
         with head():
             link(rel='stylesheet', type='text/css', href='main.css')
         with body() as b:
-            b.append(some_xml_ETree)
-            with html.div() as x:
+            with div():
                 p('This is text')
+            # No append required
+            dynamic_subtree(0, 'test')
+            # Requires append because of different Context
+            b.append(split_context(1, 'more text'))
+
 
 Page.root # Access to the generated ETree
 print(Page(doctype='html'))
-
 '''
 
 import contextlib
